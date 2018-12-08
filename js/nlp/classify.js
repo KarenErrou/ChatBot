79dfc309213b7b7195a7393b621b00f7995f5a4b
juop <juliette.opdenplatz@outlook.de>
2018-12-08 22:42:25 +0100

    Add classification of reviews

---
+++ js/nlp/classify.js
@@ -0,0 +1,65 @@
+var bayes = require('./bayes.js');
+
+/* graphdb approach does not work since data is too big */
+//var graphdb = require('../graphdb/index.js');
+//var q = "PREFIX schema: <http://schema.org/>"+
+//	"SELECT ?r ?text"+
+//	"WHERE {"+
+//	"    ?r schema:reviewBody ?text ."+
+//	"}"; 
+//
+//graphdb.query({query:q}, function(results){
+//
+//	console.log(results);
+//
+//	results.results.bindings.forEach(function(e){
+//		var emotion = bayes.classify(e.text.value);
+//		console.log(emotion);
+//	});
+//});
+
+const fs = require('fs');
+
+var mc = require('../../data/metacritic/movies.json');
+
+mc.forEach(function(movie){
+	
+	try {
+		var relations = [];
+		var reviews = require('../../data/metacritic/reviews'+movie.substr(5)+'.json');
+		for (var i=0; i<reviews["review-text"].length; i++) {
+			
+			relations.push({
+				emotion: bayes.classify(reviews["review-text"][i]),
+				movie: movie.substr(5),
+				user: reviews["review-user"][i]
+			});
+		}
+		fs.writeFileSync('../../data/metacritic/emotions'+movie.substr(5)+'.json', JSON.stringify(relations));
+
+	} catch (e) {
+		console.log(e);
+	}
+});
+
+var imdb = require('../../data/imdb/movies.json');
+
+imdb.forEach(function(movie){
+	
+	try {
+		var relations = [];
+		var reviews = require('../../data/imdb/reviews/'+movie+'.json');
+		for (var i=0; i<reviews["review-text"].length; i++) {
+			
+			relations.push({
+				emotion: bayes.classify(reviews["review-text"][i]),
+				movie: movie,
+				user: reviews["review-user"][i]
+			});
+		}
+		fs.writeFileSync('../../data/imdb/emotions/'+movie+'.json', JSON.stringify(relations));
+
+	} catch (e) {
+		console.log(e);
+	}
+});
