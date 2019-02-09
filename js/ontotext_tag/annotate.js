93ae948c83221d0241b140bdf50a2370d986c7d8
juop <juliette.opdenplatz@outlook.de>
2019-02-09 21:37:51 +0100

    Improve ontotext tagging service api

--- js/ontotext_tag/annotate.js
+++ js/ontotext_tag/annotate.js
@@ -14,9 +14,31 @@ exports.annotate = function(params, callback){
 		type: 'post'
 	})
 	.done(function(result) {
-		callback(JSON.parse(result));
+		callback(null, JSON.parse(result));
 	})
 	.fail(function(xhr) {
-		console.log(xhr);
+        callback(xhr, null);
+	});
+}
+
+exports.annotateAsync = function(params){
+
+	return new Promise(function(resolve, reject){ 
+		najax({
+			beforeSend: function(req) {
+				req.setRequestHeader("Accept", "application/vnd.ontotext.ces.document+json");
+				req.setRequestHeader("X-JwtToken", token);
+			},
+			url: 'https://tag.ontotext.com/ces-en/extract',
+			contentType: 'text/plain',
+			data: params.text,
+			type: 'post'
+		})
+		.done(function(result) {
+			resolve(JSON.parse(result));
+		})
+		.fail(function(xhr) {
+			reject(xhr);	
+		});
 	});
 }
--- js/ontotext_tag/annotate_test.js
+++ js/ontotext_tag/annotate_test.js
@@ -1,5 +1,13 @@
-var annotator = require('.annotate.js');
+var annotator = require('./annotate.js');
 
-annotator.annotate({text: "This movie is dumb. For real it is the most horrible thing that was ever made."}, function(result){
+annotator.annotate({text: "This movie is dumb. For real it is the most horrible thing that was ever made."}, function(err, result){
 	console.log(result);
+});
+
+annotator.annotateAsync({text: "This movie is dumb. For real it is the most horrible thing that was ever made."})
+.then(function(result){
+    console.log(result);
+})
+.catch(function(err){
+    console.log(err);
 });
